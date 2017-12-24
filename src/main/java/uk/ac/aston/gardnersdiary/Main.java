133ef2e5d88d39e5dc3c694ec9d0f813a3b09d01
Denver Fernandes <sub367+gitkraken@gmail.com>
2017-12-24 11:53:50 +0530

    Add support for uploading images of garden

--- src/main/java/uk/ac/aston/gardnersdiary/Main.java
+++ src/main/java/uk/ac/aston/gardnersdiary/Main.java
@@ -2,17 +2,18 @@ package uk.ac.aston.gardnersdiary;
 
 import org.javalite.activejdbc.Base;
 import uk.ac.aston.gardnersdiary.controllers.*;
-import uk.ac.aston.gardnersdiary.models.garden.Garden;
-import uk.ac.aston.gardnersdiary.services.database.GardenRetrievalJDBC;
+
+import java.io.File;
 
 import static spark.Spark.*;
+
 /**
  * Created by Denver on 15/10/2017.
  */
 public class Main {
     private static String INDEX_PATH  = "/";
     private static String IMPORT_GARDEN_PATH = "/importgarden";
-    private static String GARDEN_PATH = "/managegarden";
+    private static String MANAGE_GARDEN_PATH = "/managegarden";
     private static String PLANT_INFO_PATH = "/plantinfo/:plantname";
     private static String TASKS_PATH = "/tasks";
     private static String PLANTS_PATH = "/plants";
@@ -29,8 +30,9 @@ public class Main {
 
     private static void setupRoutes() {
         get(INDEX_PATH, IndexController.getInstance().getIndex);
-        get(GARDEN_PATH, GardenController.getInstance().getManageGarden);
+        get(MANAGE_GARDEN_PATH, GardenController.getInstance().getManageGarden);
         get(IMPORT_GARDEN_PATH, GardenController.getInstance().getImportGarden);
+        post(IMPORT_GARDEN_PATH, GardenController.getInstance().postImportGarden);
         get(PLANT_INFO_PATH, PlantInformationController.getInstance().getIndex);
         get(TASKS_PATH, TaskController.getInstance().getIndex);
         get(PLANTS_PATH, PlantsController.getInstance().getIndex);
@@ -39,6 +41,13 @@ public class Main {
     private static void setupSpark() {
         port(4567);
         staticFiles.location("/public");
+        setupUploadsDir();
         staticFiles.expireTime(600L);
+    }
+
+    private static void setupUploadsDir() {
+        File uploadDir = new File("upload");
+        uploadDir.mkdir(); // create the upload directory if it doesn't exist
+        staticFiles.externalLocation("upload");
     }
 }
--- src/main/java/uk/ac/aston/gardnersdiary/controllers/GardenController.java
+++ src/main/java/uk/ac/aston/gardnersdiary/controllers/GardenController.java
@@ -1,11 +1,24 @@
 package uk.ac.aston.gardnersdiary.controllers;
 
+import com.mysql.jdbc.StringUtils;
+import jdk.internal.util.xml.impl.Input;
 import spark.Response;
 import spark.Route;
 import spark.Request;
+import spark.utils.IOUtils;
 
+import javax.servlet.MultipartConfigElement;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.MultipartConfig;
+import javax.servlet.http.Part;
+import java.io.*;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.StandardCopyOption;
 import java.util.HashMap;
 import java.util.Map;
+
+import static spark.Spark.staticFiles;
 
 /**
  * Created by Denver on 15/10/2017.
@@ -24,7 +37,7 @@ public class GardenController extends Controller {
     public Route getManageGarden = (Request request, Response response) -> {
         Map<String, Object> attributes = new HashMap();
         attributes.put("title", "Manage Garden");
-        return renderView(request, attributes, "garden");
+        return renderView(request, attributes, "managegarden");
     };
 
     public Route getImportGarden = (Request request, Response response) -> {
@@ -32,5 +45,42 @@ public class GardenController extends Controller {
         attributes.put("title", "Import Garden");
         return renderView(request, attributes, "importGarden");
     };
+
+    public Route postImportGarden = (Request request, Response response) -> {
+        request.attribute("org.eclipse.jetty.multipartConfig", new MultipartConfigElement("C:/HashiCorp"));
+        String name = request.queryParams("name");
+        InputStream file = request.raw().getPart("file").getInputStream();
+        if(validationSucessful(name, file)) {
+            String fileUploadOutput = saveGardenImageFile(request);
+            return fileUploadOutput;
+        } else {
+
+        }
+
+        //If not sucessful redirect to error page?
+        return saveGardenImageFile(request);
+    };
+
+    public boolean validationSucessful(String name, InputStream file) {
+        if(StringUtils.isNullOrEmpty(name)) {
+            return false;
+        }
+        if(file == null) {
+            return false;
+        }
+        return true;
+    }
+
+    private String saveGardenImageFile(Request request) throws IOException, ServletException {
+        File uploadDir = new File("upload");
+        Path tempFile = Files.createTempFile(uploadDir.toPath(), "", "");
+
+        request.attribute("org.eclipse.jetty.multipartConfig", new MultipartConfigElement("/temp"));
+
+        try (InputStream input = request.raw().getPart("file").getInputStream()) { // getPart needs to use same "name" as input field in form
+            Files.copy(input, tempFile, StandardCopyOption.REPLACE_EXISTING);
+        }
+        return "<h1>You uploaded this image:<h1><img src='" + tempFile.getFileName() + "'>";
+    }
 
 }
