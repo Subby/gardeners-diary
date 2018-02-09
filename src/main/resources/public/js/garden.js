cf8e4b6174aa2d2d624728d02c56e631dd3b8dd3
Denver Fernandes <sub367@gmail.com>
2018-02-09 12:19:20 +0000

    Add partial support for deserialising saved region data

--- src/main/resources/public/js/garden.js
+++ src/main/resources/public/js/garden.js
@@ -4,8 +4,13 @@ var c1Position;
 var currentRect;
 var drag = false;
 var rectClick = false;
-var stage = setupKonvaStage();
-var layer = new Konva.Layer();
+var stage = new Konva.Stage({
+    container: 'canvas',
+    width: width,
+    height: height
+});
+var layer = generateBaseLayer();
+var imageLayer = new Konva.Layer();
 var tooltipLayer = new Konva.Layer();
 
 var imageObj = new Image();
@@ -14,18 +19,17 @@ function showModal(value) {
 	$("#modal-toggle").prop('checked', value);
 }
 
+function generateBaseLayer() {
+    if(gardenJSON) {
+        return Konva.Node.create(gardenJSON);
+    }
+    return new Konva.Layer();
+}
+
 function removeCurrentRect() {
   currentRect.destroy();
   layer.draw();
   currentRect = null;
-}
-
-function setupKonvaStage() {
-    return new Konva.Stage({
-        container: 'canvas',
-        width: width,
-        height: height
-    });
 }
 
 imageObj.onload = function() {
@@ -34,7 +38,8 @@ imageObj.onload = function() {
 	y: 50,
 	height: 250,
 	width: 500,
-	image: imageObj
+	image: imageObj,
+    id: 'gardenImage'
   });
 
   layer.on('mousedown', function() {
@@ -52,7 +57,6 @@ imageObj.onload = function() {
         drag = false;
         showModal(true);
 	}
-
   });
 
   layer.on('mousemove', function() {
@@ -74,15 +78,43 @@ imageObj.onload = function() {
       alpha: 0.75,
       visible: false
   });
+  imageLayer.add(gardenImage);
   // add the shape to the layer
-  layer.add(gardenImage);
+  if(!gardenJSON) {
+      layer.add(gardenImage);
+  }
+
   tooltipLayer.add(tooltip);
-  
+
+  stage.add(imageLayer);
   // add the layer to the stage
   stage.add(layer);
   //add layer for tooltips
   stage.add(tooltipLayer);
-  
+
+  stage.get('#region').on("mousemove", function() {
+      var mousePos = stage.getPointerPosition();
+      tooltip.position({
+          x : mousePos.x + 5,
+          y : mousePos.y + 5
+      });
+      tooltip.text(this.attrs.regionName);
+      tooltip.show();
+      tooltipLayer.batchDraw();
+      stage.draw();
+  });
+  stage.get('#region').on("mousedown", function() {
+      drag = false;
+      rectClick = true;
+      window.location.replace("/plant/");
+
+  });
+  stage.get('#region').on("mouseout", function() {
+      tooltip.hide();
+      tooltipLayer.draw();
+      stage.draw();
+  });
+
   function createNewRectangle() {
 	  var rect = new Konva.Rect({
 		x: c1Position.x,
@@ -91,27 +123,8 @@ imageObj.onload = function() {
 		height: 1,
 		stroke: 'black',
 		strokeWidth: 2,
-		id: 'Rect'
-	  });
-	  rect.on("mousemove", function() {
-	  	var mousePos = stage.getPointerPosition();
-	  	tooltip.position({
-			x : mousePos.x + 5,
-			y : mousePos.y + 5
-	  	});
-	  	tooltip.text("Some text");
-	  	tooltip.show();
-	  	tooltipLayer.batchDraw();
-	  });
-      rect.on("mousedown", function() {
-          drag = false;
-          rectClick = true;
-          window.location.replace("/plant/");
-
-      });
-	  rect.on("mouseout", function() {
-	  	tooltip.hide();
-	  	tooltipLayer.draw();
+		id: 'region',
+        regionName: 'someName'
 	  });
 	return rect;
   }
@@ -119,7 +132,7 @@ imageObj.onload = function() {
 imageObj.src = imageFile;
 $("#canvasOutputBtn").click(function() {
   //$('#outputTextArea').val('');
-  $.post("/savegardenjson", { json:stage.toJSON()} ,function(data){
+  $.post("/savegardenjson", { json:layer.toJSON()} ,function(data){
       if(data === "success") {
           $.toast({
               heading: 'Success',
